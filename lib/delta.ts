f986a383fee0a264464203807e5f140d451c9f13
Jason Laster <jason.laster.11@gmail.com>
2022-12-31 18:06:37 -0800

    Start linking to delta.replay.io

--- lib/delta.ts
+++ lib/delta.ts
@@ -1,4 +1,4 @@
 import { Project } from "./supabase";
 
 export const getDeltaBranchUrl = (project: Project, branchName: string) =>
-  `https://replay-visuals.vercel.app/project/${project.short}/?branch=${branchName}`;
+  `https://delta.replay.io/project/${project.short}/?branch=${branchName}`;

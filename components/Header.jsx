b80103fa585869b57b3ad07cd9287894d9b7c2a3
Jason Laster <jason.laster.11@gmail.com>
2023-01-02 15:32:39 -0800

    Add favico

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,7 +13,7 @@ export function Header({
   return (
     <div className="flex text-black justify-between border-b-2 mb-1 border-b-slate-100 ">
       <div className="flex items-center py-2 pl-4">
-        <div style={{ transform: "rotate(-90deg)" }} className="mr-2">
+        <div className="mr-2">
           <Image width={16} height={16} src="/logo.svg" alt="Replay logo" />
         </div>
         <Dropdown
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
+import Head from "next/head";
+
 import "../styles/globals.css";
 import { Auth } from "@supabase/ui";
 import { supabase } from "../lib/initSupabase";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Auth.UserContextProvider supabaseClient={supabase}>
-      <Component {...pageProps} />
-    </Auth.UserContextProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favico.svg" />
+      </Head>
+      <Auth.UserContextProvider supabaseClient={supabase}>
+        <Component {...pageProps} />
+      </Auth.UserContextProvider>
+    </>
   );
 }

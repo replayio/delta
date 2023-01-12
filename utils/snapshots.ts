1fe75906c6b7d98b2ef9c3cc733d87eb4bb24c3b
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-01-12 13:57:52 -0500

    Remove unused utils file

--- utils/snapshots.ts
+++
@@ -1,5 +0,0 @@
-import { Snapshot } from "../lib/server/supabase/supabase";
-
-export function changedSnapshots(snapshots: Snapshot[]): Snapshot[] {
-  return snapshots.filter((snapshot) => snapshot.primary_changed);
-}

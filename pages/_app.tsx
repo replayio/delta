fff082c39124d4cc23024e8fac26d9eef52e42e2
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-02-02 19:19:04 -0500

    Add error boundary; handle multiple branch name matches (#28)
    * Add error boundary UI
    * Handle multiple Branches matching name (find only the most recent, open result)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import "../styles/globals.css";
 import { Auth } from "@supabase/ui";
 import { supabase } from "../lib/initSupabase";
+import { ErrorBoundary } from "react-error-boundary";
+import Error from "../components/Error";
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -11,7 +13,9 @@ export default function MyApp({ Component, pageProps }) {
         <link rel="shortcut icon" href="/favico.svg" />
       </Head>
       <Auth.UserContextProvider supabaseClient={supabase}>
-        <Component {...pageProps} />
+        <ErrorBoundary FallbackComponent={Error}>
+          <Component {...pageProps} />
+        </ErrorBoundary>
       </Auth.UserContextProvider>
     </>
   );

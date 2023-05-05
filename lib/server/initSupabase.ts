d8d0d40eb9f0724bc12d1515d744979a6aedcaf0
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-05-05 09:28:17 -0400

    Fix Supabase key error

--- lib/server/initSupabase.ts
+++ lib/server/initSupabase.ts
@@ -4,6 +4,8 @@ import { config } from "dotenv";
 config({ path: "./.env.local" });
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY as string;
+const supabaseKey =
+  process.env.SUPABASE_SERVICE_KEY ??
+  (process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);

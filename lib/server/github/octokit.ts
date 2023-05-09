98e0ad8d993b5786c499633eef1f113cd28dbb43
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-05-09 13:44:50 -0400

    Retry failed GitHub API requests (#44)
    * Install @octokit/auth-app and @octokit/plugin-throttling
    * Update lib/server/github/octokit to retry GitHub API requests after failure

--- lib/server/github/octokit.ts
+++ lib/server/github/octokit.ts
@@ -1,23 +1,56 @@
 import { App } from "@octokit/app";
+import { createAppAuth } from "@octokit/auth-app";
+import { throttling } from "@octokit/plugin-throttling";
+import { Octokit } from "@octokit/rest";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env.local" });
 
-export async function getOctokit() {
-  const pem = process.env.PEM!;
-  const appId = 274973;
-  const organization = "replayio";
-  const repository = "devtools";
+const privateKey = process.env.PEM!;
+const appId = 274973;
+const organization = "replayio";
+const repository = "devtools";
 
-  const app = new App({ appId, privateKey: pem });
+export async function getOctokit() {
+  const installationId = await getInstallationId();
+
+  const ThrottledOctokit = Octokit.plugin(throttling);
+  const octokit = new ThrottledOctokit({
+    auth: {
+      appId,
+      privateKey,
+      installationId,
+    },
+    authStrategy: createAppAuth,
+    throttle: {
+      onRateLimit: (retryAfter, options, octokit, retryCount) => {
+        octokit.log.warn(
+          `Request quota exhausted for request ${options.method} ${options.url}`
+        );
+
+        if (retryCount < 2) {
+          octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+          return true;
+        }
+      },
+      onSecondaryRateLimit: (retryAfter, options, octokit) => {
+        octokit.log.warn(
+          `SecondaryRateLimit detected for request ${options.method} ${options.url}`
+        );
+      },
+    },
+  });
+
+  return octokit;
+}
+
+async function getInstallationId() {
+  const app = new App({ appId, privateKey });
 
   // First we need to get the installation id for the repo
   const { data: installation } = await app.octokit.request(
     `GET /repos/${organization}/${repository}/installation`
   );
 
-  // Then we can get an octokit instance for the installation
-  const octokit = await app.getInstallationOctokit(installation.id);
-
-  return octokit;
+  return installation.id;
 }

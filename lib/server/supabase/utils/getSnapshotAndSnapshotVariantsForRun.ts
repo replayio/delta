df6d093e4b4f875b8b465bf6bad3458354baf1d5
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-05-21 16:53:27 -0400

    Removed sequential loading of snapshots -> snapshot variants

--- lib/server/supabase/utils/getSnapshotAndSnapshotVariantsForRun.ts
+++ lib/server/supabase/utils/getSnapshotAndSnapshotVariantsForRun.ts
@@ -1,35 +1,51 @@
 import { RunId, Snapshot, SnapshotVariant } from "../../../types";
-import { getSnapshotVariantsForSnapshot } from "../tables/SnapshotVariants";
-import { getSnapshotsForRun } from "../tables/Snapshots";
+import { supabase } from "../../initSupabase";
+import { VariantToSnapshotVariant } from "../../types";
+import { assertQueryResponse } from "../supabase";
 
 export type SnapshotAndSnapshotVariants = {
   snapshot: Snapshot;
-  snapshotVariants: {
-    [variant: string]: SnapshotVariant;
-  };
+  snapshotVariants: VariantToSnapshotVariant;
 };
 
 export async function getSnapshotAndSnapshotVariantsForRun(runId: RunId) {
-  const snapshots = await getSnapshotsForRun(runId);
+  const records = await assertQueryResponse<
+    Snapshot & {
+      snapshot_variants: SnapshotVariant[];
+    }
+  >(
+    () =>
+      supabase
+        .from("snapshots")
+        .select("*, snapshot_variants(*)")
+        .eq("run_id", runId)
+        .order("delta_test_filename", {
+          ascending: true,
+        })
+        .order("delta_test_name", {
+          ascending: true,
+        })
+        .order("delta_image_filename", {
+          ascending: true,
+        })
+        .order("delta_variant", {
+          ascending: true,
+          foreignTable: "snapshot_variants",
+        }),
+    `Could not find SnapshotVariants for Run "${runId}"`
+  );
 
-  const snapshotAndSnapshotVariants: SnapshotAndSnapshotVariants[] =
-    await Promise.all(
-      snapshots.map((snapshot) =>
-        getSnapshotVariantsForSnapshot(snapshot.id)
-          .then((snapshotVariants) => {
-            const map = {};
-            snapshotVariants.forEach((snapshotVariant) => {
-              map[snapshotVariant.delta_variant] = snapshotVariant;
-            });
+  return records.map((record) => {
+    const { snapshot_variants: snapshotVariantsArray, ...snapshot } = record;
 
-            return map;
-          })
-          .then((snapshotVariants) => ({
-            snapshot,
-            snapshotVariants,
-          }))
-      )
-    );
+    const snapshotVariants: VariantToSnapshotVariant = {};
+    snapshotVariantsArray.forEach((snapshotVariant) => {
+      snapshotVariants[snapshotVariant.delta_variant] = snapshotVariant;
+    });
 
-  return snapshotAndSnapshotVariants;
+    return {
+      snapshot,
+      snapshotVariants,
+    };
+  });
 }
